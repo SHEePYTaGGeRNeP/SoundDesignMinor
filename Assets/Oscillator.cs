27ce726a89a79515152392b389a3000654d18fc1
LORENZO-PC\Lorenzo <lorenzo_suiker@hotmail.com>
2017-01-14 11:55:59 +0100

    Added Sound Design Project

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class Oscillator : MonoBehaviour
+    {
+
+        [SerializeField]
+        private double frequency = 440;
+
+        private double increment;
+        private double phase;
+        private readonly double sampling_freq = 40000;
+
+        [SerializeField]
+        private float gain = 0.15f;
+        [SerializeField]
+        private float volume = 0.1f;
+
+        private float[] frequencies;
+        private int thisFreq;
+
+        private void Start()
+        {
+            this.frequencies = new float[8] { 440, 494, 554, 587, 659, 740, 831, 880 };
+            this.gain = 0;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                this.gain = this.volume;
+                this.frequency = this.frequencies[this.thisFreq];
+                this.thisFreq ++;
+                this.thisFreq = this.thisFreq % this.frequencies.Length;
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                this.gain = 0;
+            }
+        }
+
+        private void OnAudioFilterRead(float[] data, int channels)
+        {
+            this.increment = this.frequency * 2.0 * Mathf.PI / this.sampling_freq;
+            for (int i = 0; i < data.Length; i += channels)
+            {
+
+                this.phase += this.increment;
+                data[i] = (float)(this.gain * Mathf.Sin((float)this.phase));
+                // if we have multiple speakers, play it on both
+                if (channels == 2)
+                    data[i + 1] = data[i];
+                if (this.phase > Mathf.PI * 2)
+                    this.phase = 0;
+            }
+        }
+
+
+
+    }
+}
---
+++ Assets/Spectrum.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class Spectrum : MonoBehaviour
+    {
+        public GameObject prefab;
+        public int numberOfObjects = 20;
+        public float radius = 5f;
+        public GameObject[] cubes;
+        public int multiplyValue = 250;
+
+        private void Start()
+        {
+            // Instantiates a prefab in a circle
+            for (int i = 0; i < this.numberOfObjects; i++)
+            {
+                float angle = i * Mathf.PI * 2 / this.numberOfObjects;
+                Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * this.radius;
+                Instantiate(this.prefab, pos, Quaternion.identity, this.transform);
+            }
+            this.cubes = GameObject.FindGameObjectsWithTag("Cubes");
+        }
+
+        private void Update()
+        {
+            float[] spectrum = new float[1024];
+            AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Hamming);
+            for (int i = 0; i < this.numberOfObjects; i++)
+            {
+                Vector3 previousScale = this.cubes[i].transform.localScale;
+                previousScale.y = Mathf.Lerp(previousScale.y, spectrum[i] * this.multiplyValue, Time.deltaTime * 30);
+                this.cubes[i].transform.localScale = previousScale;
+            }
+
+        }
+    }
+}
